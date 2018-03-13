7ff71a067ffcaa06ed1146c83edc1b029f90ec2b
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-03-13 13:56:47 +0100

    Create getters in highScore

--- Bomb.java
+++ Bomb.java
@@ -1,5 +1,6 @@
+import java.util.*;
 class Bomb {
-    List<Square> squares = new ArrayList;
+    List<Square> squares = new ArrayList<Square>();
     Integer x;
     Integer y;
     String sign = "@";
@@ -7,5 +8,9 @@ class Bomb {
     public Bomb (Integer x, Integer y) {
         this.x = x;
         this.y = y;
+    }
+
+    public static void main(String [] args) {
+
     }
 }
---
+++ View.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class View {
+    Square [][] boardPlayer1;
+    Square [][] boardPlayer2;
+
+    public void displayBoard() {
+
+    }
+
+    //public List<Integer> getFirstPosition() {
+
+    //}
+
+    //public Boolean getIsHorizontal() {
+
+    //}
+
+}
